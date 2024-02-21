13c4378db1c78f2e4ae529fab3a3336668b18e25
amro412 <102470851+amro412@users.noreply.github.com>
2024-02-21 11:53:48 +0200

    Delete demo/src/main/java/com/example/demo directory

--- demo/src/main/java/com/example/demo/Repository/IDepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IDepartmentRepository extends JpaRepository<Employee, Integer> {
-
-}
